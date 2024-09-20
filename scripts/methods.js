8373c519530f68b997e0d1d85cd6423d9b7e6ed6
İsmet Murat ONAY <imuratony@gmail.com>
2024-09-20 20:13:42 +0300

    http convert

--- scripts/methods.js
+++ scripts/methods.js
@@ -415,7 +415,7 @@ function setContactMeInfo() {
 
 function loggedUserInfo(callback) {
      try {
-          fetch('https://freeipapi.com/api/json')
+          fetch('http://freeipapi.com/api/json')
                .then(response => response.json())
                .then(data => {
                     senderInfo = data;
