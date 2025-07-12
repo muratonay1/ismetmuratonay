e2df6ae3fa1cf67a9cc3a341b8cab668d0732f75
İsmet Murat ONAY <imuratony@gmail.com>
2025-07-12 11:17:08 +0300

    fix

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
-let baseUrl = 'http://localhost:3000/api/'
 const token = '65d78580994151d94460ea1f'
 const isDeveloperMode = false;
+let baseUrl = isDeveloperMode ? 'http://localhost:3000/api/' : "https://api.muratonay.com.tr/api/"
 let EndPoints = {
      API_GET_COUNTER     :    'api-get-counter',
      API_VERIFY_CAPTCHA  :    'api-verify-captcha',
